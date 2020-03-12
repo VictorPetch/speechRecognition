3c5a3d8d6c0dc8e80f5b7e2ef54c35f5a821bb2e
victorpetch <victorpetch06@gmail.com>
2020-03-12 09:37:58 -0300

    Finished making the csv with the tokens

--- mlpTrain.py
+++ mlpTrain.py
@@ -1,1 +1,6 @@
 ###TRAINING MLP TO COMPREEND TEXT MEANING###
+import numpy as np 
+import pandas as pd 
+
+dataset = pd.read_csv('wordsTokens.csv')
+print('dataset shape: ', dataset.shape)
--- onehotEncoding.py
+++ onehotEncoding.py
@@ -1,12 +1,28 @@
-###One Hot Encoding the csv###
+###For now it receives a dictionary, tokenize it and one hot encodes it###
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.preprocessing import OneHotEncoder
 import numpy as np
-enc = OneHotEncoder(handle_unknown='ignore')
-#print(np.asarray(X).shape)
-X = [['bring', 'me','a','coke','please'],['coke', 'please','can','i','have'],['i', 'would','like','a','coke']]
-#X = X.reshape(-1,1)
-#print(X.shape)
-enc.fit(X)
-print(enc.categories_[1])
-print(enc.transform(X).toarray())
+import csv
+corpus = [
+          'Can you bring me a coke please',
+          'bring me a coke please',
+          'I want some coke ',
+          'Can I have a coke',
+          'I want a coke',
+          'Can you bring me a pepsi please',
+          'bring me a pepsi please',
+          'I want some pepsi',
+          'I want a pepsi',
+          'I would like a pepsi please',
+          'I would like a coke please',
+         ]
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(corpus)
+header = np.asarray([vectorizer.get_feature_names()])
+#print(vectorizer.get_feature_names())
+X = X.toarray()
+data = np.append(header,X,axis = 0)
+destination = 'wordsTokens.csv'
+with open(destination, mode='a') as f_file:
+    f_file = csv.writer(f_file, delimiter=',', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
+    for row in data:
+        f_file.writerow(row)
